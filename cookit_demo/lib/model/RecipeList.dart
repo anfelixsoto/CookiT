f805ec9678b28005addd8e581dada40887258c03
JuliaHeleneW <gyros.chips@gmail.com>
2019-11-19 17:57:56 -0800

    API recipe search and detailed recipe view without filtering

---
+++ cookit_demo/lib/model/RecipeList.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:cookit_demo/model/recipeId.dart';
+
+import 'package:http/http.dart' as http;
+
+String getUrlString(List<String> ingredients){
+    String request='';
+    for(int i=0;i<ingredients.length;i++){
+      if(i==(ingredients.length-1)){
+        request+=ingredients[i];
+      }
+      else{
+        request+=(ingredients[i]+',+');
+      }
+    }
+    return request;
+  }
+
+class RecipeList{
+  final List<RecipeId> recipes;
+  RecipeList({this.recipes});
+
+  factory RecipeList.fromJSON(List<dynamic> json){
+    return RecipeList(recipes:json.map((p) => RecipeId.fromJSON(p)).toList());
+  }
+
+
+  static Future<RecipeList> fetchRecipes(List<String> ingredients) async {
+    String url=getUrlString(ingredients);
+    final response =
+      await http.get('https://api.spoonacular.com/recipes/findByIngredients?ingredients='+url+'&number=2&apiKey=18743cb58f294573a49a41d78c78a8ce');
+
+      if (response.statusCode == 200) {
+        // If the call to the server was successful, parse the JSON.
+        return RecipeList.fromJSON(json.decode(response.body));
+          } else {
+            // If that call was not successful, throw an error.
+            throw Exception('Failed to load post');
+          }
+  }
+}
+
+

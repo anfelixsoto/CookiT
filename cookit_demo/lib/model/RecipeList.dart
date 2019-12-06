57efafcee803625a6ce32a9962db718bbe5051b9
JuliaHeleneW <gyros.chips@gmail.com>
2019-12-06 01:42:37 -0800

    Added recipe search screen, wired it to app, added recipe validation

--- cookit_demo/lib/model/RecipeList.dart
+++ cookit_demo/lib/model/RecipeList.dart
@@ -29,7 +29,7 @@ class RecipeList{
   static Future<RecipeList> fetchRecipes(List<String> ingredients) async {
     String url=getUrlString(ingredients);
     final response =
-      await http.get('https://api.spoonacular.com/recipes/findByIngredients?ingredients='+url+'&number=2&apiKey=18743cb58f294573a49a41d78c78a8ce');
+      await http.get('https://api.spoonacular.com/recipes/findByIngredients?ingredients='+url+'&ranking=2&number=5&apiKey=d6c4887150dc409191eebfd9378ee595');
 
       if (response.statusCode == 200) {
         // If the call to the server was successful, parse the JSON.
@@ -40,6 +40,5 @@ class RecipeList{
           }
   }
 }
-
 
 

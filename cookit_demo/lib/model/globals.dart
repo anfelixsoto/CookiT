1245a1f7f242207c629a3777fd7f0d78e4864a30
JuliaHeleneW <gyros.chips@gmail.com>
2019-12-12 14:33:10 -0800

    made API keys array global

--- cookit_demo/lib/model/globals.dart
+++ cookit_demo/lib/model/globals.dart
@@ -1,4 +1,14 @@
 class Globals{
+
+  final List<String> apiKeys=['1e3d8937a2294ffd9e5730315989629f'];
+
+  static String getKey(){
+    Globals g=Globals.instance;
+    int c=g.counter;
+    g.incrementCounter();
+    return g.apiKeys[c];
+  }
+
   int counter;
   Globals._privateConstructor(){
     counter=0;
@@ -7,8 +17,9 @@ class Globals{
   static final Globals _instance = Globals._privateConstructor();
 
   static Globals get instance { return _instance;}
+
   void incrementCounter(){
-    if(counter<1){
+    if(counter<(_instance.apiKeys.length-1)){
       counter++;
     }
     else{
